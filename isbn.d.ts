956b95445311ffe5d3a7ec96bab5de551df1ecf0
Andrej Souček <andrejsoucek@gmail.com>
2026-01-12 20:25:09 +0100

    types: differentiate isbn10 and isbn13

--- isbn.d.ts
+++ isbn.d.ts
@@ -9,29 +9,39 @@ interface ISBNAudit {
   }>;
 }
 
-interface ISBN {
+interface ISBNBase {
   source: string;
-  isValid: boolean;
-  isIsbn10: boolean;
-  isIsbn13: boolean;
-  prefix?: string;
+  isValid: true;
   group: string;
   publisher: string;
   article: string;
   check: string;
-  isbn13?: string;
-  isbn13h?: string;
-  check10: string;
-  check13: string;
   groupname: string;
-  isbn10?: string;
-  isbn10h?: string;
 }
+
+interface ISBN10 extends ISBNBase {
+  isIsbn10: true;
+  isIsbn13: false;
+  check10: string;
+  isbn10: string;
+  isbn10h: string;
+}
+
+interface ISBN13 extends ISBNBase {
+  isIsbn10: false;
+  isIsbn13: true;
+  prefix: string;
+  check13: string;
+  isbn13: string;
+  isbn13h: string;
+}
+
+type ISBN = ISBN10 | ISBN13;
 
 declare module "isbn3" {
   export function parse(isbn: string): ISBN | null;
-  export function asIsbn13(isbn: string): string | null;
-  export function asIsbn10(isbn: string): string | null;
+  export function asIsbn13(isbn: string, hyphen?: boolean): string | null;
+  export function asIsbn10(isbn: string, hyphen?: boolean): string | null;
   export function hyphenate(isbn: string): string;
   export function audit(isbn: string): ISBNAudit;
   export const groups: Record<
